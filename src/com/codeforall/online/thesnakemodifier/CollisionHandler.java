05e88801820dee8bab7d342c85611d74bc6f1a5b
Francisco <franciscosimoees598@gmail.com>
2024-07-29 00:54:05 +0100

    add the logic for the collision with the snake's body

--- src/com/codeforall/online/thesnakemodifier/CollisionHandler.java
+++ src/com/codeforall/online/thesnakemodifier/CollisionHandler.java
@@ -1,4 +1,28 @@
 package com.codeforall.online.thesnakemodifier;
 
-public class CollisionHandler {
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+import java.util.LinkedList;
+
+public class CollisionHandler  {
+    /**
+     * Checks if the snake has collided with itself.
+     *
+     * @param headX The X coordinate of the snake's head.
+     * @param headY The Y coordinate of the snake's head.
+     * @param body The list of all snake segments (excluding the head).
+     * @return True if the snake has collided with itself; otherwise, false.
+     */
+
+
+    public static boolean checkSelfCollision(int headX, int headY, LinkedList<Picture> body) {
+        for (int i = 1; i < body.size(); i++) { // Start from index 1 to skip the head
+            Picture part = body.get(i);
+            if (part.getX() == headX || part.getY() == headY) {
+                return true; // Collision detected
+            }
+        }
+        return false; // No collision detected
+    }
 }
+
+
